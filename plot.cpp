b32cdc30a0b36170f63d429414db32d506d2461e
oprc <chisainux@gmail.com>
2024-06-20 15:51:05 +0700

    mmm

--- plot.cpp
+++ plot.cpp
@@ -7,19 +7,16 @@ class Plot {
 		int width, height, sleep_time;
 		int loop = 1;
 		vector<vector<vector<int>>> vec_list;
-		vector<int> vec_pos;
+		vector<vector<int>> vec_pos;
 		Plot(int w, int h, int st){
 			width = w;
 			height = h;
 			sleep_time = st;
 		}
 		void addVec(vector<vector<int>> vec, vector<int> pos){
-		//	try {
-		//	vec_list.insert(vec_list.end(), vec.begin(), vec.end());
-			vec_pos.insert(vec_pos.end(), pos.begin(), pos.end());
+		//	vec_pos.insert(vec_pos.end(), pos.begin(), pos.end());
+			vec_pos.push_back(pos);
 			vec_list.push_back(vec);
-		//	vec_pos.push_back(pos.at(pos.begin()));
-		//} catch{}}
 }
 		void showVec(){
 			for(int i=0;i<vec_list.size();i++){
@@ -27,7 +24,7 @@ class Plot {
 					for(int k=0;k<vec_list.at(i).at(j).size();k++){
 						cout << vec_list.at(i).at(j).at(k) << " ";
 					}
-					cout << endl;
+					 cout << endl;
 				}
 			}
 		}
@@ -36,23 +33,55 @@ class Plot {
 				for(int py=height;py>=-height;py--){
 					for(int px=-width;px<=width;px++){
 						int pointed = 0;
-						// load vec_list
+						int b = 0;
+						int w_x = 0;
 						for(int i=0;i<vec_list.size();i++){
 							for(int j=0;j<vec_list.at(i).size();j++){
 								for(int k=0;k<vec_list.at(i).at(j).size();k++){
-									if(px==vec_pos.at(i) && py==vec_pos.at(i)){
+									pointed =  vec_list[i][j][w_x];
+									b = 1;
+									break;
+								}if(b==1) break;
+							}if(b==1) break;
+							cin >> w_x;
+						}
+						//cin >> w_x;
+						w_x+=1;
+						if(w_x>5) w_x = 0;
+						// load vec_list
+						// end load vec_list
+						/*
+						for(int i=0;i<vec_list.size();i++){
+							if(pointed==1) break;
+							for(int j=0;j<vec_list.at(i).size();j++){
+								if(pointed==1) break;
+								for(int k=0;k<vec_list.at(i).at(j).size();k++){
+									if(pointed==1) break;
+									int cx = (px-vec_list.at(i).at(j).size());
+									int cy = (py-vec_list.at(i).size());
+									//if(cx==cy) cout << cx-cy <<"_";
+									//if(px-vec_pos.at(i).at(0) >0 && px-vec_pos.at(i).at(0) <= 5){
+									if(px<vec_pos.at(i).at(0) + 5 && px>=vec_pos.at(i).at(0) && py<vec_pos.at(i).at(1)+7 && py>=vec_pos.at(i).at(1)){
+										
+										//cout << vec_list.at(i).at(j).at(k);
+										if(pointed==1) break;
 										if(vec_list.at(i).at(j).at(k)==1){
-											pointed = 1;	
+											pointed = 1;
+											//cout << vec_list.at(i).at(j).at(k);
+											break;
 										}
 									}
 								}
 							}
-						}
+						}*/
 						if(pointed==1) cout <<"+";
 						else cout << " ";
+						//cout << " ";
+						pointed =0;
 					}
 					cout << "| " << py << endl;
 				}
+						showVec();
 				usleep(sleep_time*10000);
 				system("clear");
 			}while(loop==1);
